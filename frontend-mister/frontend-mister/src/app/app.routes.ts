10e0ae6bd5df01968a88459fa553c5ec20b0c07a
DelPradoPC <josemanuelgarciadelprado@gmail.com>
2025-08-04 19:58:50 +0200

    Cambios frontend

---
+++ frontend-mister/frontend-mister/src/app/app.routes.ts
@@ -0,0 +1,11 @@
+import { Routes } from '@angular/router';
+import { LoginComponent } from './features/auth/login/login.component';
+import { DashboardComponent } from './features/dashboard/dashboard.component';
+import { RegisterComponent } from './features/auth/register/register.component';
+
+export const routes: Routes = [
+    {path:'', redirectTo:'login', pathMatch:'full'},
+    {path:'dashboard', component:DashboardComponent},
+    {path:'login', component:LoginComponent},
+    {path:'register', component:RegisterComponent}
+];
---
+++ frontend-mister/frontend-mister/src/app/features/player-create/player-create.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { PlayerCreate } from './player-create';
+
+describe('PlayerCreate', () => {
+  let component: PlayerCreate;
+  let fixture: ComponentFixture<PlayerCreate>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [PlayerCreate]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(PlayerCreate);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
